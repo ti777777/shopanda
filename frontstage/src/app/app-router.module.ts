6a19672408aeca4f30d5b1e31abe103f5966c57e
wenpang <fgh2026291@gmail.com>
2020-12-12 18:05:50 +0800

    modify frontstage

--- frontstage/src/app/app-router.module.ts
+++ frontstage/src/app/app-router.module.ts
@@ -3,14 +3,19 @@ import { CommonModule } from '@angular/common';
 import {RouterModule , Routes} from '@angular/router';
 
 /*引入组件*/
-import { ShopandaIndexComponent } from './components/shopanda-index/shopanda-index.component';
+import { IndexComponent } from './components/index/index.component';
+import { LoginComponent} from './components/login/login.component';
+import { ProductComponent } from './components/product/product.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 const appRouters: Routes = [
-  { path: 'index' , component: ShopandaIndexComponent }
+  { path: '' , component: IndexComponent },
+  { path: 'login' , component: LoginComponent },
+  { path: 'products' , component: ProductComponent },
+  { path: 'register' , component: RegisterComponent },
+  { path: 'shoppingcart' , component: ShoppingCartComponent }
 ];
-
-
-
 
 @NgModule({
   declarations: [],
--- frontstage/src/app/app.module.ts
+++ frontstage/src/app/app.module.ts
@@ -2,18 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRouterModule } from './app-router.module';
+import { IndexComponent } from './components/index/index.component';
+import { ProductComponent } from './components/product/product.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { ElModule } from 'element-angular';
 
 /*引入组件*/
-import { ShopandaIndexComponent } from './components/shopanda-index/shopanda-index.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShopandaIndexComponent
+    IndexComponent,
+    ProductComponent,
+    ShoppingCartComponent,
+    LoginComponent,
+    RegisterComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRouterModule
+    AppRouterModule,
+    ElModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
